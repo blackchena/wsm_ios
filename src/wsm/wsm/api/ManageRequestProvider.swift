6b4ff850a4a53562645c4bc7ca4733f1c5de7e3b
daolq <le.quang.dao@framgia.com>
2017-10-31 09:32:14 +0700

    task/#144646 Feature manage list logic P1

---
+++ src/wsm/wsm/api/ManageRequestProvider.swift
@@ -0,0 +1,93 @@
+//
+//  ManageRequestProvider.swift
+//  wsm
+//
+//  Created by DaoLQ on 10/31/17.
+//  Copyright © 2017 framgia. All rights reserved.
+//
+
+import Foundation
+import Moya
+import PromiseKit
+
+fileprivate enum ManageRequestApiEndPoint: BaseApiTargetType {
+    case getListManageRequest(requestType: RequestType, manageRequestApiInput: ManageRequestApiInputModel)
+    case handleRequest(requestType: RequestType, handleRequestType: HandleRequestType, requestIds: [Int])
+    
+    public var path: String {
+        switch self {
+        case .getListManageRequest(let requestType, _):
+            switch requestType {
+            case RequestType.others:
+                return "/api/dashboard/manager/request_leaves"
+            case .overTime:
+                return "/api/dashboard/manager/request_ots"
+            case .dayOff:
+                return "/api/dashboard/manager/request_offs"
+            }
+        case .handleRequest(_, _, _):
+            return "/api/dashboard/manager/approvals"
+        }
+    }
+    
+    public var method: Moya.Method {
+        switch self {
+        case .getListManageRequest(_):
+            return .get
+        case .handleRequest(_):
+            return .put
+        }
+    }
+    
+    public var parameters: [String : Any]? {
+        switch self {
+        case .handleRequest(let requestType, let handleRequestType, let requestIds):
+            var request: String {
+                switch requestType {
+                case .others:
+                    return "leave"
+                case .overTime:
+                    return "off"
+                case .dayOff:
+                    return "ot"
+                }
+            }
+            
+            switch handleRequestType {
+            case .approveSingleRequest:
+                return ["request": request, "status": "approve", "request_id": requestIds[0]]
+            case .approveMultiRequest:
+                return ["request": request, "approve_all": true, "request_ids": requestIds]
+            case .rejectSingleRequest:
+                return ["request": request, "status": "discard", "request_id": requestIds[0]]
+            }
+        case .getListManageRequest(_, let manageRequestApiInput):
+            return createParameters(fromDataModel: manageRequestApiInput)
+        }
+    }
+}
+
+enum HandleRequestType {
+    case approveSingleRequest
+    case approveMultiRequest
+    case rejectSingleRequest
+}
+
+final class ManageRequestProvider {
+    static let shared  = ManageRequestProvider()
+    var leaveRequests = [RequestLeaveModel]()
+    var otRequests = [RequestOtModel]()
+    var offRequests = [RequestDayOffModel]()
+
+    static func getListManageLeaveRequest(requestType: RequestType, manageRequestApiInput: ManageRequestApiInputModel) -> Promise<ManageLeaveRequestApiOutputModel> {
+        return ApiProvider.shared.requestPromise(target: MultiTarget(ManageRequestApiEndPoint.getListManageRequest(requestType: requestType, manageRequestApiInput: manageRequestApiInput)))
+    }
+    
+    static func getListManageOverTimeRequest(requestType: RequestType, manageRequestApiInput: ManageRequestApiInputModel) -> Promise<ManageOvertimeRequestApiOutputModel> {
+        return ApiProvider.shared.requestPromise(target: MultiTarget(ManageRequestApiEndPoint.getListManageRequest(requestType: requestType, manageRequestApiInput: manageRequestApiInput)))
+    }
+    
+    static func getListManageOffRequest(requestType: RequestType, manageRequestApiInput: ManageRequestApiInputModel) -> Promise<ManageOffRequestApiOutputModel> {
+        return ApiProvider.shared.requestPromise(target: MultiTarget(ManageRequestApiEndPoint.getListManageRequest(requestType: requestType, manageRequestApiInput: manageRequestApiInput)))
+    }
+}
--- src/wsm/wsm/viewcontrollers/ManageRequestListViewController.swift
+++ src/wsm/wsm/viewcontrollers/ManageRequestListViewController.swift
@@ -13,11 +13,17 @@ class ManageRequestListViewController: BaseViewController {
 
     @IBOutlet weak var tableView: UITableView!
         
-    var requestType: RequestType?
+    var requestType: RequestType
+    var manageRequestApiInput = ManageRequestApiInputModel()
+    
+    fileprivate var currentPage = 1
+    fileprivate var isLoading = false
+    fileprivate let refreshControl = UIRefreshControl()
+
     
     required init(type: RequestType) {
-        super.init(nibName: "ManageRequestListViewController", bundle: nil)
         self.requestType = type
+        super.init(nibName: "ManageRequestListViewController", bundle: nil)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -27,11 +33,109 @@ class ManageRequestListViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        navigationItem.title = requestType?.title
+        navigationItem.title = requestType.title
         
         tableView.delegate = self
         tableView.dataSource = self
         tableView?.register(UINib(nibName: "ManageRequestTableViewCell", bundle: nil), forCellReuseIdentifier: "ManageRequestTableViewCell")
+        
+        // Add Refresh Control to Table View
+        refreshControl.addTarget(self, action: #selector(pullToRefreshHandler(_:)), for: .valueChanged)
+        refreshControl.tintColor = .appBarTintColor
+        if #available(iOS 10.0, *) {
+            tableView.refreshControl = refreshControl
+        } else {
+            tableView.addSubview(refreshControl)
+        }
+        
+        getListManageRequest(page: currentPage)
+    }
+    
+    @objc private func pullToRefreshHandler(_: Any) {
+        getListManageRequest()
+    }
+    
+    func getListManageRequest(page: Int = 1) {
+        if isLoading {
+            return
+        }
+        AlertHelper.showLoading()
+        isLoading = true
+        switch requestType {
+        case .others:
+            getLeaveRequests(page: page)
+        case .overTime:
+            getOvertimeRequests(page: page)
+        case .dayOff:
+            getDayOffRequests(page: page)
+        }
+        
+    }
+    
+    private func getLeaveRequests(page: Int) {
+        ManageRequestProvider.getListManageLeaveRequest(requestType: requestType, manageRequestApiInput: manageRequestApiInput)
+            .then {apiOutput -> Void in
+                if page > 1 {
+                    if apiOutput.leaveRequests.count > 0 {
+                        ManageRequestProvider.shared.leaveRequests.append(contentsOf: apiOutput.leaveRequests)
+                        self.tableView.reloadData()
+                    }
+                } else {
+                    ManageRequestProvider.shared.leaveRequests = apiOutput.leaveRequests
+                    self.tableView.reloadData()
+                }
+                self.currentPage = page
+            }.catch { error in
+                AlertHelper.showError(error: error)
+            }.always {
+                AlertHelper.hideLoading()
+                self.refreshControl.endRefreshing()
+                self.isLoading = false
+        }
+    }
+    
+    private func getOvertimeRequests(page: Int) {
+        ManageRequestProvider.getListManageOverTimeRequest(requestType: requestType, manageRequestApiInput: manageRequestApiInput)
+            .then {apiOutput -> Void in
+                if page > 1 {
+                    if apiOutput.overtimeRequests.count > 0 {
+                        ManageRequestProvider.shared.otRequests.append(contentsOf: apiOutput.overtimeRequests)
+                        self.tableView.reloadData()
+                    }
+                } else {
+                    ManageRequestProvider.shared.otRequests = apiOutput.overtimeRequests
+                    self.tableView.reloadData()
+                }
+                self.currentPage = page
+            }.catch { error in
+                AlertHelper.showError(error: error)
+            }.always {
+                AlertHelper.hideLoading()
+                self.refreshControl.endRefreshing()
+                self.isLoading = false
+        }
+    }
+    
+    private func getDayOffRequests(page: Int) {
+        ManageRequestProvider.getListManageOffRequest(requestType: requestType, manageRequestApiInput: manageRequestApiInput)
+            .then {apiOutput -> Void in
+                if page > 1 {
+                    if apiOutput.offRequests.count > 0 {
+                        ManageRequestProvider.shared.offRequests.append(contentsOf: apiOutput.offRequests)
+                        self.tableView.reloadData()
+                    }
+                } else {
+                    ManageRequestProvider.shared.offRequests = apiOutput.offRequests
+                    self.tableView.reloadData()
+                }
+                self.currentPage = page
+            }.catch { error in
+                AlertHelper.showError(error: error)
+            }.always {
+                AlertHelper.hideLoading()
+                self.refreshControl.endRefreshing()
+                self.isLoading = false
+        }
     }
 }
 
@@ -45,12 +149,36 @@ extension ManageRequestListViewController: UITableViewDelegate, UITableViewDataS
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // TODOs: update later
-        return 20
+        switch requestType {
+        case .others:
+            return ManageRequestProvider.shared.leaveRequests.count
+        case .overTime:
+            return ManageRequestProvider.shared.otRequests.count
+        case .dayOff:
+            return ManageRequestProvider.shared.offRequests.count
+        }
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 75.0
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        switch requestType {
+        case .others:
+            if indexPath.row == ManageRequestProvider.shared.leaveRequests.count - 1 {
+                getListManageRequest(page: currentPage + 1)
+            }
+        case .overTime:
+            if indexPath.row == ManageRequestProvider.shared.otRequests.count - 1 {
+                getListManageRequest(page: currentPage + 1)
+            }
+        case .dayOff:
+            if indexPath.row == ManageRequestProvider.shared.offRequests.count - 1 {
+                getListManageRequest(page: currentPage + 1)
+            }
+        }
+        
     }
     
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
